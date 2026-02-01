465c85eb4bf76081c059128ba6286aa0a205605e
ahnafmizan7 <75119665+ahnafmizan7@users.noreply.github.com>
2026-02-01 18:26:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You like me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "Itna pasand krte ho mujhe???🥰🚀💝",  // Shows when they go past 5000%
         high: "Yayyy you like me so muchh",              // Shows when they go past 1000%
-        normal: "Itna pasand krte ho mujhe???🥰"                           // Shows when they go past 100%
+        normal: "Damn kreji🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dmcmvjrlm/video/upload/v1769988180/SANAM_-_Gulabi_Aankhen_jspltf.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
